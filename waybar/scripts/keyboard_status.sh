5d22a1fc6a4e09eee98bde3b6497084469836d1e
otgursop <otgursop@git.com>
2026-07-05 00:06:40 +0500

    updated

--- waybar/scripts/keyboard_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-layout=$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap')
-case $layout in
-    "English (US)") echo "EN" ;;
-    "Russian") echo "RU" ;;
-    "German") echo "DE" ;;
-    *) echo "$layout" ;;
-esac
